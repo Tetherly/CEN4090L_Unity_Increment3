4227e8ce8c3f9af5d5119981be7adf70ae217b91
Nathan Zhao <nzhao123@gmail.com>
2022-02-22 19:27:06 +0000

    Intial Key Strike Sound  Script

---
+++ Assets/Scripts/KeyNoteScript.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyNoteScript : MonoBehaviour
+{
+    public AudioSource C4;
+    public AudioSource D4;
+    public AudioSource E4;
+    public AudioSource F4;
+    public AudioSource G4;
+    public AudioSource A4;
+    public AudioSource B4;
+    public AudioSource C5;
+    public AudioSource D5;
+    public AudioSource E5;
+    public AudioSource F5;
+    public AudioSource G5;
+    public AudioSource A5;
+    public AudioSource B5;
+
+    public void C4_Stroke()
+    {
+        C4.Play();
+    }
+    public void D4_Stroke()
+    {
+        D4.Play();
+    }
+    public void E4_Stroke()
+    {
+        E4.Play();
+    }
+    public void F4_Stroke()
+    {
+        F4.Play();
+    }
+    public void G4_Stroke()
+    {
+        G4.Play();
+    }
+    public void A4_Stroke()
+    {
+        A4.Play();
+    }
+    public void B4_Stroke()
+    {
+        B4.Play();
+    }
+
+    public void C5_Stroke()
+    {
+        C5.Play();
+    }
+    public void D5_Stroke()
+    {
+        D5.Play();
+    }
+    public void E5_Stroke()
+    {
+        E5.Play();
+    }
+    public void F5_Stroke()
+    {
+        F5.Play();
+    }
+    public void G5_Stroke()
+    {
+        G5.Play();
+    }
+    public void A5_Stroke()
+    {
+        A5.Play();
+    }
+    public void B5_Stroke()
+    {
+        B5.Play();
+    }
+}
