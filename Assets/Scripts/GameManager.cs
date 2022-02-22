9d3838bd0f0a45fd48efb185b8f08ea6ebf27886
Tyler <tylerstarling@outlook.com>
2022-02-22 20:00:46 +0000

    No Comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,70 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+           
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.DoubleQuote))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+        }
+
+
+
     }
 }
