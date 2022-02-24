eaa8a12d4846bdfcbd7b8158e1120779b28af7c9
Tyler <tylerstarling@outlook.com>
2022-02-24 19:41:03 +0000

    Added basics for the NoteSpawner and the Spawn Locations for the keys. Also added a music note art asset and prefab for testing the spawning.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,70 +14,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.CapsLock))
-        {
-            // Play Audio Clip for Key
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-           
-        }
-
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.Semicolon))
-        {
-            
-        }
-        if (Input.GetKeyDown(KeyCode.DoubleQuote))
-        {
-            
-        }
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            
-        }
-
-
-
+       
     }
 }
