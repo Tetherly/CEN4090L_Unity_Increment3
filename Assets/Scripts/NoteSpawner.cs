5a2fbc29bed4415a8e1ee0db5914eb310df0caf1
Tyler <tylerstarling@outlook.com>
2022-03-08 19:12:37 +0000

    Implemented the ground work for the spawning of music notes into the scene. Notes spawn at random locations on a time interval.

--- Assets/Scripts/NoteSpawner.cs
+++ Assets/Scripts/NoteSpawner.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class NoteSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public Transform[] spawnPoints;
-    public GameObject[] notePrefabs;
+    [SerializeField]
+    private Transform[] spawnPoints;
+    [SerializeField]
+    private GameObject[] notePrefabs;
     void Start()
     {
-        
+        InvokeRepeating("SpawnMusicNotes", 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            int randomNote = Random.Range(0, notePrefabs.Length);
-            int randNoteSpawn = Random.Range(0, spawnPoints.Length);
+       
+    }
 
-            Instantiate(notePrefabs[0], spawnPoints[randNoteSpawn].position, transform.rotation);
-        }
+    private void SpawnMusicNotes()
+    {
+        int randomNote = Random.Range(0, notePrefabs.Length);
+        int randNoteSpawn = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(notePrefabs[0], spawnPoints[randNoteSpawn].position, transform.rotation);
+        
     }
 }
