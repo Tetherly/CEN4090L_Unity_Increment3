ef3164ae471758e202d3039255ee3ac666421583
Talal Khaled Mahmoudi <talalmah@yahoo.com>
2022-03-27 18:14:42 +0000

    Implemented scoring for all white and black keys, still needs work

--- Assets/Scripts/Demo.cs
+++ Assets/Scripts/Demo.cs
@@ -17,9 +17,9 @@ public class Demo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.CapsLock) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.G) || Input.GetKey(KeyCode.H) || Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.K) || Input.GetKey(KeyCode.L) || Input.GetKey(KeyCode.Semicolon) || Input.GetKey(KeyCode.Quote) || Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.T) || Input.GetKey(KeyCode.Y) || Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.O) || Input.GetKey(KeyCode.LeftBracket) || Input.GetKey(KeyCode.RightBracket))
         {
-            scoreTracker.ScoreTracker += 100;
+            scoreTracker.ScoreTracker += 1;
         }
     }
     
